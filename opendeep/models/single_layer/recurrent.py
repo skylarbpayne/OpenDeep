eb834c1ddf893538fb7043fde03b57a7cd3b1e49
Skylar Payne <skylar.b.payne@gmail.com>
2015-04-19 19:43:29 -0700

    added standard RNN layer

--- opendeep/models/single_layer/recurrent.py
+++ opendeep/models/single_layer/recurrent.py
@@ -25,105 +25,111 @@ log = logging.getLogger(__name__)
 class RecurrentLayer(Model):
     """
     Your run-of-the-mill recurrent model. Normally not as good as LSTM/GRU, but it is simplest.
+    
+    TODO: update description
     """
     default = {
         'activation': 'tanh', #type of activation function to use for output
+        'cost': 'mse',
+        'cost_args': {},
         'weights_init': 'gaussian', #either 'gaussian' or 'uniform' - how to initialize weights
         'weights_mean': 0, #mean for gaussian weights init
         'weights_std': 0.005, #standard deviation for gaussian weights init
         'weights_interval': 'montreal', # if the weights_init was 'uniform' how to initialize from uniform
         'bias_init': 0.0, #how to initialize the bias parameter
-        'output_t0_init': 0.0
+        'input_size': None,
+        'output_size': None
     }
 
     def __init__(self, inputs_hook=None, config=None, defaults=default, params_hook=None,
                 input_size=None, output_size=None, activation=None, weights_init=None,
-                weights_mean=None, weights_std=None, weights_interval=None, bias_init=None, output_t0_init=None):
+                weights_mean=None, weights_std=None, weights_interval=None, bias_init=None,
+                transition=None, taps={0: [-1], -1: [0]}, backwards=False, **kwargs):
         #init Model to combine defaults and config dictionaries.
-        super(RecurrentLayer, self).__init__(config, defaults)
+        super(RecurrentLayer, self).__init__(**{arg: val for (arg, val) in locals().iteritems() if arg is not 'self'})
         # all configuration parameters are now in self.args
 
         ##################
         # specifications #
         ##################
         # grab info from the inputs_hook, or from parameters
-        if inputs_hook: # inputs_hook is a tuple of (Shape, Input)
+        if inputs_hook is not None: # inputs_hook is a tuple of (Shape, Input)
             assert len(inputs_hook) == 2 # make sure inputs_hook is a tuple
-            input_size = inputs_hook[0] or input_size
-            self.input = inputs_hook[1]
+            self.input_size = inputs_hook[0] or self.input_size
+            self.input = self.inputs_hook[1]
         else:
-            #either grab from the parameter directly or self.args config
-            input_size = input_size or self.args.get('input_size')
             # make the iput a symbolic matrix
             self.input = T.fmatrix('X')
-        # either grab from the paramter directly, self.args config, or copy n_in
-        output_size = output_size or self.args.get('output_size') or input_size
+       
+        #RNN can take in inputs from multiple sources. Store in a list:
+        if type(self.input) is not list:
+            self.input = [self.input]
+            self.input_size = [self.input_size]
 
-        # other specifications
-        weights_init = weights_init or self.args.get('weights_init')
-        #for gaussian weights
-        mean = weights_mean or self.args.get('weights_mean')
-        std = weights_std or self.args.get('weights_std')
-        # for uniform weights
-        interval = weights_interval or self.args.get('weights_interval')
-        # for bias
-        bias_init = bias_init or self.args.get('bias_init')
-        # for initial output setting
-        output_t0_init = output_t0_init or self.args.get('output_t0_init')
+        #now that we have input specs, define output 'target' variable for supervised training
+        self.target = T.fmatrix('Y')
+
+        # either grab from the paramter directly, self.args config, or copy n_in
+        output_size = self.output_size or self.input_size
+        
+        self.taps = taps
+        if 0 not in self.taps:
+            self.taps[0] = []
 
         #activation function
-        activation_name = activation or self.args.get('activation')
-        if isinstance(activation_name, basestring):
+        if isinstance(self.activation, basestring):
             activation_func = get_activation_function(activation_name)
         else:
-            assert callable(activation_name)
-            activation_func = activation_name
+            assert callable(self.activation)
+            activation_func = self.activation
+        
+#transition model here?
+
+        #cost function
+        #if a string name was given, look up the correct function
+        if isinstance(self.cost, basestring):
+            cost_func = get_cost_function(self.cost)
+        #otherwise, if a callable (custom function) was passed:
+        else:
+            assert callable(self.cost), "Cost function either needs to be a string name or callable!"
+            cost_func = self.cost
+
 
         ####################################################
         # parameters - make sure to deal with params_hook! #
         ####################################################
-        if params_hook:
-            assert len(params_hook) == 4, "Expected 4 params (Wx, W, output_t0, and b) for RecurrentLayer, found {0!s}".format(
-                    len(params_hook)) # make sure the params_hook has Wx, W, and b
-            Wx, W, output_t0, b = params_hook
+        #you can either specify a transition function, or a basic layer will be used by default.
+        if transition is not None:
+            raise NotImplementedError("Deep Transitions for RNN is not yet implemented")
         else:
-            # if we are initializing weights from a gaussian
-            if weights_init.lower() == 'gaussian':
-                Wx = get_weights_gaussian(shape=(input_size, output_size), mean=mean, std=std, name="Wx")
-                W = get_weights_gaussian(shape=(output_size, output_size), mean=mean, std=std, name="W")
-            # if we are initializing weights from a uniform distribution
-            elif weights_init.lower() == 'uniform':
-                Wx = get_weights_uniform(shape=(input_size, output_size), mean=mean, std=std, name="Wx")
-                W = get_weights_uniform(shape=(output_size, output_size), mean=mean, std=std, name="W")
-            # otherwise not implemented
-            else:
-                log.error("Did not recognize weights_init %s! Please try gaussian or uniform" % 
-                            str(weights_init))
-                raise NotImplementedError("Did not recognize weights_init %s! Please try gaussian or uniform" %
-                            str(weights_init))
-
-            b = get_bias(shape=output_size, name="b", init_values=bias_init)
-            #NOTE: Since output_t0 is usually initialized similar to biases in the literature,
-            # I used get_bias for initialization
-            output_t0 = get_bias(shape=output_size, name="output_t0", init_values=output_t0_init)
-        # Finally have the three parameters!
-        self.params = [Wx, W, output_t0, b]
+            #need to find the input_size for the basic layer
+            # this is essentially sum of the # taps * input_size
+            # but we don't know anything about any other layers from here.
+            # how much work do we shift to the user?
+            #for an initial go, let's just use size of input and size of output
+            all_sizes = [self.output_size] + self.input_size
+            transition_input_size = sum([all_sizes[k] * len(v) for k,v in self.taps]) 
+            
+            transition_args = {arg: val for (arg, val) in locals().iteritems() if arg is not 'self'}
+            transition_args['input_size'] = transition_input_size
+            self.transition = BasicLayer(**transition_args)
+        #Nothing about the params_hook because that's taken care of in transition
 
         ###############
         # computation #
         ###############
-        #This defines a single recurrent step where x_t is the current input, and o_tm1 is the output from
-        # the previous step.
-        def step(x_t, o_tm1):
-            return T.tanh(T.dot(x_t, Wx) + T.dot(o_tm1, W) + b)
+        self.output, _ = theano.scan(self._step,
+                    sequences=[dict(input=self.input[k], taps=v) for k,v in self.taps if k is not 0], #first dimension for all inputs should be Time
+                    outputs_info=[dict(initial=T.zeros_like(self.output), taps=self.taps[0])], #The output at the time step before your first input
+                    go_backwards=backwards)
 
-        output, _ = theano.scan(step,
-                    sequences=self.input, #First dimension of self.input should be time.
-                    outputs_info=output_t0) #The output at the time step before your first input
-        
-        self.output = output
+        self.cost = cost_func(output=self.output, target=self.target, **self.cost_args) 
         log.debug("Initialized a recurrent, fully-connected layer with shape %s and activation: %s" %
                     (str((input_size, output_size)), str(activation_name)))
+
+    def _step(self, *args):
+        full_input = T.concatenate(args)
+        return self.transition.predict(full_input)
 
     def get_inputs(self):
         return [self.input]
@@ -131,8 +137,17 @@ class RecurrentLayer(Model):
     def get_outputs(self):
         return self.output
 
+    def get_targets(self):
+        return [self.target]
+    
+    def get_train_cost(self):
+        return self.cost
+
     def get_params(self):
         return self.params
+
+    def save_args(self, args_file="recurrentlayer_config.pkl"):
+        super(RecurrentLayer, self).save_args(args_file)
 
 class LSTM(Model):
     """
@@ -140,13 +155,15 @@ class LSTM(Model):
     https://github.com/lisa-lab/DeepLearningTutorials/blob/master/code/lstm.py
     """
     # TODO: LSTM recurrent model
-    log.error("LSTM not implemented!")
-    raise NotImplementedError("LSTM not implemented!")
+    def __init__(self):
+        log.error("LSTM not implemented!")
+        raise NotImplementedError("LSTM not implemented!")
 
 class GRU(Model):
     """
     Gated recurrent units.
     """
     # TODO: GRU recurrent model
-    log.error("GRU not implemented!")
-    raise NotImplementedError("GRU not implemented!")
+    def __init__(self):
+        log.error("GRU not implemented!")
+        raise NotImplementedError("GRU not implemented!")
